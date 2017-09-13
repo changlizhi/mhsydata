96da987a630c7cb5780492a458f523afbdf52afe
changlz <changlizhi@aliyun.com>
2017-09-13 12:56:56 +0800

    提供查询全部的yewus-test

--- tests/Jueseyewus_test.go
+++ tests/Jueseyewus_test.go
@@ -1,37 +1,42 @@
 package tests
-
 import (
-	"changliang/zfzhi"
-	"log"
-	"mhsydata/moxings"
-	"mhsydata/zdjueseyewus"
-	"testing"
+"mhsydata/moxings"
+"changliang/zfzhi"
+"mhsydata/zdjueseyewus"
+"log"
+"testing"
+
 )
-
-func TestJueseyewusTianjia(t *testing.T) {
-	juese := moxings.Juese{
-		Biaoji:    "JueseTianjia",
-		Bianma:    "JueseTianjia",
-		Mingcheng: "JueseTianjia",
-		Id:        zfzhi.Zhi.Shuzi1(),
-	}
-	zdjueseyewus.Tianjiajuese(&juese)
-}
-
-func TestJueseyewusXiugai(t *testing.T) {
-	juese := moxings.Juese{
-		Biaoji:    "JueseXiugai",
-		Bianma:    "JueseXiugai",
-		Mingcheng: "JueseXiugai",
-		Id:        zfzhi.Zhi.Shuzi1(),
-	}
-	zdjueseyewus.Xiugaijuese(&juese)
-}
-func TestJueseyewusChaxun(t *testing.T) {
-	juese := zdjueseyewus.Chaxunjuese(1)
-	log.Println(juese)
+func TestJueseyewusTianjia(t *testing.T){
+juese:=moxings.Juese{
+Mingcheng:"JueseTianjia",
+Id:zfzhi.Zhi.Shuzi1(),
+Biaoji:"JueseTianjia",
+Bianma:"JueseTianjia",
 
 }
-func TestJueseyewusShanchu(t *testing.T) {
-	zdjueseyewus.Shanchujuese(1)
+zdjueseyewus.Tianjiajuese(&juese)
+}
+
+func TestJueseyewusXiugai(t *testing.T){
+juese:=moxings.Juese{
+Id:zfzhi.Zhi.Shuzi1(),
+Biaoji:"JueseXiugai",
+Bianma:"JueseXiugai",
+Mingcheng:"JueseXiugai",
+
+}
+zdjueseyewus.Xiugaijuese(&juese)
+}
+func TestJueseyewusChaxun(t *testing.T){
+juese:=zdjueseyewus.Chaxunjuese(zfzhi.Zhi.Shuzi1())
+log.Println(juese)
+
+}
+func TestJueseyewusShanchu(t *testing.T){
+zdjueseyewus.Shanchujuese(zfzhi.Zhi.Shuzi1())
+}
+func TestJueseyewusChaxunquanbu(t *testing.T){
+quanbu:=zdjueseyewus.Chaxunquanbujuese()
+log.Println("quanbu[zfzhi.Zhi.Shuzi0()]:====",quanbu[zfzhi.Zhi.Shuzi0()])
 }
