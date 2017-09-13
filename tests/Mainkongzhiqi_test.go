b70b0eaa1b98351e5cf564742d7be75c7d84ab20
changlz <changlizhi@aliyun.com>
2017-09-13 14:23:38 +0800

    把控制器的参数设置挪到了工具中

--- tests/Mainkongzhiqi_test.go
+++ tests/Mainkongzhiqi_test.go
@@ -1,31 +1,20 @@
 package tests
+import(
+"mhsydata/zdkongzhiqis"
+"gongju"
+"changliang/zf"
+"changliang/zfzhi"
+"log"
+"strconv"
+"testing"
 
-import (
-	"changliang/zf"
-	"changliang/zfzhi"
-	"github.com/astaxie/beego/context"
-	"gongju"
-	"log"
-	"mhsydata/zdkongzhiqis"
-	"strconv"
-	"testing"
 )
-
-func mainkongzhiqi() *zdkongzhiqis.Mainkongzhiqi {
-	c := zdkongzhiqis.Mainkongzhiqi{}
-	c.Data = make(map[interface{}]interface{})
-	c.Ctx = context.NewContext()
-	c.Ctx.Input = context.NewInput()
-	c.Ctx.Output = context.NewOutput()
-	c.Ctx.Output.Context = context.NewContext()
-	c.Ctx.Output.Context.ResponseWriter = &context.Response{new(gongju.Mywriter), true, 200}
-	return &c
-}
-func TestGetmain(t *testing.T) {
-	paramid := strconv.Itoa(zfzhi.Zhi.Shuzi1())
-	c := mainkongzhiqi()
-	c.Ctx.Input.SetParam(zfzhi.Zhi.Mh()+zf.Zfs.Id(false), paramid)
-	c.Get()
-	log.Println(c.Data[zf.Zfs.Json(true)])
+func TestGetmain(t *testing.T){
+paramid:=strconv.Itoa(zfzhi.Zhi.Shuzi1())
+c:=zdkongzhiqis.Mainkongzhiqi{}
+gongju.Kongzhiqi(&c.Controller)
+c.Ctx.Input.SetParam(zfzhi.Zhi.Mh()+zf.Zfs.Id(false),paramid)
+c.Get()
+log.Println(c.Data[zf.Zfs.Json(true)])
 
 }
