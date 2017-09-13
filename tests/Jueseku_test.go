5368b2cbf585274daa45b4588370a2bd0606f430
changlz <changlizhi@aliyun.com>
2017-09-13 12:56:31 +0800

    fmt

--- tests/Jueseku_test.go
+++ tests/Jueseku_test.go
@@ -1,55 +1,53 @@
 package tests
-
-import (
-	"mhsydata/moxings"
-	"testing"
-	"log"
-	"changliang/zfzhi"
-	"mhsydata/zdjuesekus"
+import(
+"mhsydata/moxings"
+"testing"
+"log"
+"changliang/zfzhi"
+"mhsydata/zdjuesekus"
 )
-
-func TestTianjiaduogeJuese(t *testing.T) {
-	juese2 := moxings.Juese{
-		Biaoji:"BiaojiTianjiaduogeTest2",
-		Bianma:"BianmaTianjiaduogeTest2",
-		Mingcheng:"MingchengTianjiaduogeTest2",
-		Id:zfzhi.Zhi.Shuzi1(),
-	}
-	juese3 := moxings.Juese{
-		Bianma:"BianmaTianjiaduogeTest3",
-		Mingcheng:"MingchengTianjiaduogeTest3",
-		Id:zfzhi.Zhi.Shuzi1(),
-		Biaoji:"BiaojiTianjiaduogeTest3",
-	}
-	jueses := []moxings.Juese{juese2, juese3}
-	zdjuesekus.Tianjiaduoge(jueses)
+func TestTianjiaduogeJuese(t *testing.T){
+juese2:=moxings.Juese{
+Mingcheng:"MingchengTianjiaduogeTest2",
+Id:zfzhi.Zhi.Shuzi1(),
+Biaoji:"BiaojiTianjiaduogeTest2",
+Bianma:"BianmaTianjiaduogeTest2",
 }
-func TestTianjiayigeJuese(t *testing.T) {
-	juese := &moxings.Juese{
-		Id:zfzhi.Zhi.Shuzi1(),
-		Biaoji:"BiaojiTianjiayigeTest1",
-		Bianma:"BianmaTianjiayigeTest1",
-		Mingcheng:"MingchengTianjiayigeTest1",
-	}
-	zdjuesekus.Tianjiayige(juese)
+juese3:=moxings.Juese{
+Mingcheng:"MingchengTianjiaduogeTest3",
+Id:zfzhi.Zhi.Shuzi1(),
+Biaoji:"BiaojiTianjiaduogeTest3",
+Bianma:"BianmaTianjiaduogeTest3",
 }
-func TestXiugaiyigeJuese(t *testing.T) {
-	juese := &moxings.Juese{
-		Mingcheng:"MingchengXiugaiyigeTest1",
-		Id:zfzhi.Zhi.Shuzi1(),
-		Biaoji:"BiaojiXiugaiyigeTest1",
-		Bianma:"BianmaXiugaiyigeTest1",
-	}
-	zdjuesekus.Xiugaiyige(juese)
+jueses:=[]moxings.Juese{juese2,juese3}
+zdjuesekus.Tianjiaduoge(jueses)
 }
-func TestChaxunyigeJuese(t *testing.T) {
-	juese := zdjuesekus.Chaxunyige(zfzhi.Zhi.Shuzi1())
-	log.Println(juese)
+func TestTianjiayigeJuese(t *testing.T){
+juese:=&moxings.Juese{
+Bianma:"BianmaTianjiayigeTest1",
+Mingcheng:"MingchengTianjiayigeTest1",
+Id:zfzhi.Zhi.Shuzi1(),
+Biaoji:"BiaojiTianjiayigeTest1",
 }
-func TestShanchuyigeJuese(t *testing.T) {
-	zdjuesekus.Shanchuyige(zfzhi.Zhi.Shuzi1())
+zdjuesekus.Tianjiayige(juese)
 }
-func TestChaxunquanbuJuese(t *testing.T) {
-	all := zdjuesekus.Chaxunquanbu()
-	log.Println("all[zfzhi.Zhi.Shuzi0()]:====", all[zfzhi.Zhi.Shuzi0()])
+func TestXiugaiyigeJuese(t *testing.T){
+juese:=&moxings.Juese{
+Id:zfzhi.Zhi.Shuzi1(),
+Biaoji:"BiaojiXiugaiyigeTest1",
+Bianma:"BianmaXiugaiyigeTest1",
+Mingcheng:"MingchengXiugaiyigeTest1",
+}
+zdjuesekus.Xiugaiyige(juese)
+}
+func TestChaxunyigeJuese(t *testing.T){
+juese:=zdjuesekus.Chaxunyige(zfzhi.Zhi.Shuzi1())
+log.Println(juese)
+}
+func TestShanchuyigeJuese(t *testing.T){
+zdjuesekus.Shanchuyige(zfzhi.Zhi.Shuzi1())
+}
+func TestChaxunquanbuJuese(t *testing.T){
+all:=zdjuesekus.Chaxunquanbu()
+log.Println("all[zfzhi.Zhi.Shuzi0()]:====",all[zfzhi.Zhi.Shuzi0()])
 }
