9ccd1ee390d0d122ac53a6f9e963e22e3ebc7dd6
changlz <changlizhi@aliyun.com>
2017-09-17 16:45:52 +0800

    自动生成的此文件

--- fortests/Dtziyuanfortests.go
+++ fortests/Dtziyuanfortests.go
@@ -15,25 +15,26 @@ func Zuzhuangdtziyuan(leixing string, jige int, fangfa string) []*moxings.Dtziyu
 		zfzhi.Zhi.Shuzi0(),
 	)
 	for i := zfzhi.Zhi.Shuzi1(); i <= jige; i++ {
-		istr := strconv.Itoa(i)
+		istring := strconv.Itoa(i)
 		dtziyuan := &moxings.Dtziyuan{
-			Xiugairiqi:     time.Now(),
-			Biaoji:         leixing + zf.Zfs.Biaoji(false) + fangfa + istr,
-			Mingcheng:      leixing + zf.Zfs.Mingcheng(false) + fangfa + istr,
-			Paixu:          i,
-			Bianma:         leixing + zf.Zfs.Bianma(false) + fangfa + istr,
-			Lujing:         leixing + zf.Zfs.Lujing(false) + fangfa + istr,
-			Chuangjianriqi: time.Now(),
 			Id:             i,
-			Fubianma:       leixing + zf.Zfs.Fubianma(false) + fangfa + istr,
+			Mingcheng:      leixing + zf.Zfs.Mingcheng(false) + fangfa + istring,
+			Bianma:         leixing + zf.Zfs.Bianma(false) + fangfa + istring,
+			Fubianma:       leixing + zf.Zfs.Fubianma(false) + fangfa + istring,
+			Chuangjianriqi: time.Now(),
+			Biaoji:         leixing + zf.Zfs.Biaoji(false) + fangfa + istring,
+			Paixu:          i,
+			Lujing:         leixing + zf.Zfs.Lujing(false) + fangfa + istring,
+			Xiugairiqi:     time.Now(),
 		}
 		ret = append(ret, dtziyuan)
-	}
 
+	}
 	return ret
 }
-func Zuzhuangdtziyuanstr(leixing string, jige int, fangfa string) string {
+func Zuzhuangdtziyuanstring(leixing string, jige int, fangfa string) string {
 	ret := Zuzhuangdtziyuan(leixing, jige, fangfa)
-	jstr, _ := json.Marshal(ret)
-	return string(jstr)
+	jstring, _ := json.Marshal(ret)
+	return string(jstring)
+
 }
