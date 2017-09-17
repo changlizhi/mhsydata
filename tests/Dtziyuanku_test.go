5e4af8e7d820524e2af197a53065601348f45d0d
changlz <changlizhi@aliyun.com>
2017-09-17 21:59:21 +0800

    参数生成从fortests生成

--- tests/Dtziyuanku_test.go
+++ tests/Dtziyuanku_test.go
@@ -1,67 +1,41 @@
 package tests
 
 import (
+	"changliang/fanshe"
+	"changliang/zf"
 	"changliang/zfzhi"
 	"log"
-	"mhsydata/moxings"
+	"mhsydata/fortests"
 	"mhsydata/zddtziyuankus"
 	"testing"
-	"time"
 )
 
 func TestTianjiaduogeDtziyuan(t *testing.T) {
-	dtziyuan2 := moxings.Dtziyuan{
-		Xiugairiqi:     time.Now(),
-		Biaoji:         "BiaojiTianjiaduogeTest2",
-		Mingcheng:      "MingchengTianjiaduogeTest2",
-		Paixu:          zfzhi.Zhi.Shuzi1(),
-		Bianma:         "BianmaTianjiaduogeTest2",
-		Lujing:         "LujingTianjiaduogeTest2",
-		Chuangjianriqi: time.Now(),
-		Id:             zfzhi.Zhi.Shuzi1(),
-		Fubianma:       "FubianmaTianjiaduogeTest2",
-	}
-	dtziyuan3 := moxings.Dtziyuan{
-		Paixu:          zfzhi.Zhi.Shuzi1(),
-		Chuangjianriqi: time.Now(),
-		Lujing:         "LujingTianjiaduogeTest3",
-		Xiugairiqi:     time.Now(),
-		Biaoji:         "BiaojiTianjiaduogeTest3",
-		Id:             zfzhi.Zhi.Shuzi1(),
-		Mingcheng:      "MingchengTianjiaduogeTest3",
-		Bianma:         "BianmaTianjiaduogeTest3",
-		Fubianma:       "FubianmaTianjiaduogeTest3",
-	}
-	dtziyuans := []moxings.Dtziyuan{dtziyuan2, dtziyuan3}
-	zddtziyuankus.Tianjiaduoge(dtziyuans)
+	dtziyuan := fortests.Zuzhuangdtziyuan(
+		zf.Zfs.Kus(false),
+		zfzhi.Zhi.Shuzi3(),
+		fanshe.Fangfaming(false),
+	)
+	zddtziyuankus.Tianjiaduoge(dtziyuan)
+
 }
 func TestTianjiayigeDtziyuan(t *testing.T) {
-	dtziyuan := &moxings.Dtziyuan{
-		Id:             zfzhi.Zhi.Shuzi1(),
-		Paixu:          zfzhi.Zhi.Shuzi1(),
-		Bianma:         "BianmaTianjiayigeTest1",
-		Chuangjianriqi: time.Now(),
-		Mingcheng:      "MingchengTianjiayigeTest1",
-		Fubianma:       "FubianmaTianjiayigeTest1",
-		Lujing:         "LujingTianjiayigeTest1",
-		Xiugairiqi:     time.Now(),
-		Biaoji:         "BiaojiTianjiayigeTest1",
-	}
-	zddtziyuankus.Tianjiayige(dtziyuan)
+	dtziyuan := fortests.Zuzhuangdtziyuan(
+		zf.Zfs.Kus(false),
+		zfzhi.Zhi.Shuzi1(),
+		fanshe.Fangfaming(false),
+	)
+	zddtziyuankus.Tianjiayige(dtziyuan[zfzhi.Zhi.Shuzi0()])
+
 }
 func TestXiugaiyigeDtziyuan(t *testing.T) {
-	dtziyuan := &moxings.Dtziyuan{
-		Paixu:          zfzhi.Zhi.Shuzi1(),
-		Chuangjianriqi: time.Now(),
-		Fubianma:       "FubianmaXiugaiyigeTest1",
-		Lujing:         "LujingXiugaiyigeTest1",
-		Xiugairiqi:     time.Now(),
-		Biaoji:         "BiaojiXiugaiyigeTest1",
-		Id:             zfzhi.Zhi.Shuzi1(),
-		Mingcheng:      "MingchengXiugaiyigeTest1",
-		Bianma:         "BianmaXiugaiyigeTest1",
-	}
-	zddtziyuankus.Xiugaiyige(dtziyuan)
+	dtziyuan := fortests.Zuzhuangdtziyuan(
+		zf.Zfs.Kus(false),
+		zfzhi.Zhi.Shuzi1(),
+		fanshe.Fangfaming(false),
+	)
+	zddtziyuankus.Xiugaiyige(dtziyuan[zfzhi.Zhi.Shuzi0()])
+
 }
 func TestChaxunyigeDtziyuan(t *testing.T) {
 	dtziyuan := zddtziyuankus.Chaxunyige(zfzhi.Zhi.Shuzi1())
