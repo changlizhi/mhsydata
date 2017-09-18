d7df807cf5d68d9cea7eaa6e4f30b7513b693b3f
changlz <changlizhi@aliyun.com>
2017-09-18 11:51:23 +0800

    fmt

--- fortests/Dtziyuanfortests.go
+++ fortests/Dtziyuanfortests.go
@@ -1,41 +1,40 @@
 package fortests
-import(
-"mhsydata/moxings"
-"time"
-"strconv"
-"changliang/zf"
-"changliang/zfzhi"
-"encoding/json"
 
+import (
+	"changliang/zf"
+	"changliang/zfzhi"
+	"encoding/json"
+	"mhsydata/moxings"
+	"strconv"
+	"time"
 )
-func Zuzhuangdtziyuan(leixing string,jige int,fangfa string)[]*moxings.Dtziyuan{
-ret:=make(
-[]*moxings.Dtziyuan,
-zfzhi.Zhi.Shuzi0(),
 
-)
-for i:=zfzhi.Zhi.Shuzi1();i<=jige;i++{
-istring:=strconv.Itoa(i)
-dtziyuan:=&moxings.Dtziyuan{
-Id:i,
-Mingcheng:leixing+zf.Zfs.Mingcheng(false)+fangfa+istring,
-Chuangjianriqi:time.Now(),
-Xiugairiqi:time.Now(),
-Biaoji:leixing+zf.Zfs.Biaoji(false)+fangfa+istring,
-Paixu:i,
-Bianma:leixing+zf.Zfs.Bianma(false)+fangfa+istring,
-Fubianma:leixing+zf.Zfs.Fubianma(false)+fangfa+istring,
-Lujing:leixing+zf.Zfs.Lujing(false)+fangfa+istring,
+func Zuzhuangdtziyuan(leixing string, jige int, fangfa string) []*moxings.Dtziyuan {
+	ret := make(
+		[]*moxings.Dtziyuan,
+		zfzhi.Zhi.Shuzi0(),
+	)
+	for i := zfzhi.Zhi.Shuzi1(); i <= jige; i++ {
+		istring := strconv.Itoa(i)
+		dtziyuan := &moxings.Dtziyuan{
+			Id:             i,
+			Mingcheng:      leixing + zf.Zfs.Mingcheng(false) + fangfa + istring,
+			Chuangjianriqi: time.Now(),
+			Xiugairiqi:     time.Now(),
+			Biaoji:         leixing + zf.Zfs.Biaoji(false) + fangfa + istring,
+			Paixu:          i,
+			Bianma:         leixing + zf.Zfs.Bianma(false) + fangfa + istring,
+			Fubianma:       leixing + zf.Zfs.Fubianma(false) + fangfa + istring,
+			Lujing:         leixing + zf.Zfs.Lujing(false) + fangfa + istring,
+		}
+		ret = append(ret, dtziyuan)
 
+	}
+	return ret
 }
-ret=append(ret,dtziyuan)
-
-}
-return ret
-}
-func Zuzhuangdtziyuanyigestring(leixing string,fangfa string)string{
-ret:=Zuzhuangdtziyuan(leixing,zfzhi.Zhi.Shuzi1(),fangfa)
-jstring,_:=json.Marshal(ret[zfzhi.Zhi.Shuzi0()])
-return string(jstring)
+func Zuzhuangdtziyuanyigestring(leixing string, fangfa string) string {
+	ret := Zuzhuangdtziyuan(leixing, zfzhi.Zhi.Shuzi1(), fangfa)
+	jstring, _ := json.Marshal(ret[zfzhi.Zhi.Shuzi0()])
+	return string(jstring)
 
 }
--- tests/Dtziyuanfortests_test.go
+++ tests/Dtziyuanfortests_test.go
@@ -1,26 +1,25 @@
 package tests
-import(
-"changliang/fanshe"
-"changliang/zf"
-"changliang/zfzhi"
-"log"
-"mhsydata/fortests"
-"testing"
 
+import (
+	"changliang/fanshe"
+	"changliang/zf"
+	"changliang/zfzhi"
+	"log"
+	"mhsydata/fortests"
+	"testing"
 )
-func TestZuzhuangdtziyuan(t *testing.T){
-jiegouti:=fortests.Zuzhuangdtziyuan(
-zf.Zfs.Test(true),
-zfzhi.Zhi.Shuzi4(),
-fanshe.Fangfaming(false),
 
-)
-log.Println("jiegouti:====",jiegouti)
-jiegoutiyigestring:=fortests.Zuzhuangdtziyuanyigestring(
-zf.Zfs.Test(true),
-fanshe.Fangfaming(false),
-
-)
-log.Println("jiegoutiyigestring:====",jiegoutiyigestring)
+func TestZuzhuangdtziyuan(t *testing.T) {
+	jiegouti := fortests.Zuzhuangdtziyuan(
+		zf.Zfs.Test(true),
+		zfzhi.Zhi.Shuzi4(),
+		fanshe.Fangfaming(false),
+	)
+	log.Println("jiegouti:====", jiegouti)
+	jiegoutiyigestring := fortests.Zuzhuangdtziyuanyigestring(
+		zf.Zfs.Test(true),
+		fanshe.Fangfaming(false),
+	)
+	log.Println("jiegoutiyigestring:====", jiegoutiyigestring)
 
 }
